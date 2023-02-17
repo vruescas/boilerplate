64cd44f254a77ffa6f23cb217a682395702fabfc
Valentín Ruescas <var.vlc@coma.de>
2023-02-17 11:18:12 +0100

    [TASK] Composer Update

--- config/system/settings.php
+++ config/system/settings.php
@@ -35,6 +35,16 @@ return [
             'loginLogo' => '',
             'loginLogoAlt' => '',
         ],
+        'bootstrap_package' => [
+            'disableCssProcessing' => '0',
+            'disableGoogleFontCaching' => '0',
+            'disablePageTsBackendLayouts' => '0',
+            'disablePageTsContentElements' => '0',
+            'disablePageTsRTE' => '0',
+            'disablePageTsTCADefaults' => '0',
+            'disablePageTsTCEFORM' => '0',
+            'disablePageTsTCEMAIN' => '0',
+        ],
         'extensionmanager' => [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
